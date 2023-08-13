1114e40a9d2d1dc99e2c7af50928f548f95665bd
muradtt011 <muradtt011@gmail.com>
2023-08-13 23:23:29 +0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Student>students = new List<Student>()
+        {
+            new(){Id=1,Name="Name1",Age=20 },
+             new(){Id=2,Name="Name2",Age=10 },
+             new(){Id=3,Name="Name3",Age=30 }
+        };                  
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+        public IActionResult About(int id)
+
+        {
+
+            return View(students.Find(s => s.Id == id));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    "default",
+    "{controller=home}/{action=index}/{id?}");
+  
+app.UseStaticFiles();
+
+app.Run();
